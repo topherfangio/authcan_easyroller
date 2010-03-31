e6b5c1c672d7ca9f52b1a341c6ad5d57d5b37ed6
Topher Fangio <fangiotophia@gmail.com>
2010-03-31 17:32:06 -0500

    Fixed indentation in example files.

--- examples/ability.rb
+++ examples/ability.rb
@@ -1,41 +1,41 @@
 class Ability
-	include CanCan::Ability
+  include CanCan::Ability
 
-	def initialize(current_user)
-		can :read, :all
-		can :manage, UserSession
+  def initialize(current_user)
+    can :read, :all
+    can :manage, UserSession
 
-		if current_user
-			# Abilities for someone with an account (does not necessarily have a "user" role)
-			can [:update, :destroy], User do |user|
-				user == current_user
-			end
-
-
-			# User role abilities
-			if current_user.is_user?
-			end
+    if current_user
+      # Abilities for someone with an account (does not necessarily have a "user" role)
+      can [:update, :destroy], User do |user|
+        user == current_user
+      end
 
 
-			# Moderator role abilities
-			if current_user.is_moderator?
-			end
+      # User role abilities
+      if current_user.is_user?
+      end
 
 
-			# Admin role abilities
-			if current_user.is_admin?
-				can :manage, :all
-			end
+      # Moderator role abilities
+      if current_user.is_moderator?
+      end
 
 
-			# Developer role abilities
-			if current_user.is_developer?
-				can :manage, :all
-			end
-		else
-			can :create, User
-		end
+      # Admin role abilities
+      if current_user.is_admin?
+        can :manage, :all
+      end
 
-	end
+
+      # Developer role abilities
+      if current_user.is_developer?
+        can :manage, :all
+      end
+    else
+      can :create, User
+    end
+
+  end
 
 end

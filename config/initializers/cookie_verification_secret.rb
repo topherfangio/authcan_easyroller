0490610fe6ba9641d9a97c858bef56eb3dcf27bc
Topher Fangio <fangiotophia@gmail.com>
2010-03-31 16:40:26 -0500

    Removing old Rails application files for engine support.

--- config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '57ae962a25bb2820a1f83239034492febcf2ee641e5afc1e52274df72b0e439bc8b1ef0f21549f594ceb68b3145a782e2c9ff3070bc17052c527aa4a8981c38a'
